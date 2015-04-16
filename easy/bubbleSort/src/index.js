f9868533aefdaeb0d898c4eb6f4f735c8eb8cca5
ripter <ripter001@gmail.com>
2015-04-15 21:55:04 -0700

    added bubbleSort challenge

---
+++ easy/bubbleSort/src/index.js
@@ -0,0 +1,17 @@
+/* global module */
+'use strict';
+/*
+ * Bubble Sort
+ * http://simple.wikipedia.org/wiki/Bubble_sort
+ * Write a bubble sort algorithm.
+ * Run the tests by typing 'make' on the command line.
+ * You are not allowed to include any libraries.
+ *
+ */
+
+function bubbleSort(list) {
+  // Write code here
+  return list;
+}
+
+module.exports = bubbleSort;
---
+++ easy/bubbleSort/test/test.js
@@ -0,0 +1,33 @@
+/*global require, describe, it */
+var should = require('should');
+var bubbleSort = require('../src/index.js');
+
+describe('Sorting a list', function() {
+  it('sorts [16, 42, 8, 4, 15, 23]', function() {
+    var list = [16, 42, 8, 4, 15, 23];
+    var sorted = [4, 8, 15, 16, 23, 42];
+
+    bubbleSort(list).should.equal(sorted);
+  });
+
+  it('sorts [0, 0, 0, 1]', function() {
+    var list = [0, 0, 0, 1];
+    var sorted = [1, 0, 0, 0];
+
+    bubbleSort(list).should.equal(sorted);
+  });
+
+  it('sorts [0, 1, 0, 1]', function() {
+    var list = [0, 1, 0, 1];
+    var sorted = [1, 1, 0, 0];
+
+    bubbleSort(list).should.equal(sorted);
+  });
+
+  it('sorts [1, 0, 0, 0]', function() {
+    var list = [1, 0, 0, 0];
+    var sorted = [1, 0, 0, 0];
+
+    bubbleSort(list).should.equal(sorted);
+  });
+});
