ab68b3ea4934e8ae76e816df78989f07cfd197d9
Chris <ripter001@gmail.com>
2015-04-17 18:59:19 -0700

    eql actually checks lists. equal does not.

--- easy/bubbleSort/test/test.js
+++ easy/bubbleSort/test/test.js
@@ -7,27 +7,27 @@ describe('Sorting a list', function() {
     var list = [16, 42, 8, 4, 15, 23];
     var sorted = [4, 8, 15, 16, 23, 42];
 
-    bubbleSort(list).should.equal(sorted);
+    bubbleSort(list).should.eql(sorted);
   });
 
   it('sorts [0, 0, 0, 1]', function() {
     var list = [0, 0, 0, 1];
     var sorted = [1, 0, 0, 0];
 
-    bubbleSort(list).should.equal(sorted);
+    bubbleSort(list).should.eql(sorted);
   });
 
   it('sorts [0, 1, 0, 1]', function() {
     var list = [0, 1, 0, 1];
     var sorted = [1, 1, 0, 0];
 
-    bubbleSort(list).should.equal(sorted);
+    bubbleSort(list).should.eql(sorted);
   });
 
   it('sorts [1, 0, 0, 0]', function() {
     var list = [1, 0, 0, 0];
     var sorted = [1, 0, 0, 0];
 
-    bubbleSort(list).should.equal(sorted);
+    bubbleSort(list).should.eql(sorted);
   });
 });
